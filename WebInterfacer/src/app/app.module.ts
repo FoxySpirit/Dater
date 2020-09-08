af07afbcf83fad531d42995e0ed5825e593bfef6
Roman Orlovsky <r.orlovsky@optimatica.ru>
2020-09-08 14:46:32 +0300

    Webinterface add

---
+++ WebInterfacer/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalendarComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatSnackBarModule
+  ],
+  providers: [
+    MatSnackBar
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
